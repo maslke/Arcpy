82eeb8ee235a366bde9b3d1bb2dbfa06f3da7ab3
maslke <maslke@outlook.com>
2016-06-04 10:58:54 +0800

    数据集，要素类相关操作

--- management/CreateFileGDB.py
+++ management/CreateFileGDB.py
@@ -7,11 +7,52 @@ import arcpy
 
 path = r'C:\\arcpy\\'
 name = 'test2.gdb'
+#create file gdb
 arcpy.CreateFileGDB_management(path,name)
+
+#spatialReference
 sr = arcpy.SpatialReference(4326)
+
+#create dataset
 arcpy.CreateFeatureDataset_management(path+name,'TEST',sr)
+
+#create featureclass
 arcpy.CreateFeatureclass_management(path+name+'\\Test','test','POLYGON')
 
-print arcpy.Exists(path+name+'\\Test\\test')
+fc = path+name+'\\Test\\test'
+
+#delete features
+arcpy.DeleteFeatures_management(fc)
+
+#delete features with cursor:
+with arcpy.da.UpdateCursor(fc,['NAME']) as cursor:
+    for row in cursor:
+        cursor.deleteRow()
+
+#get polygon extent
+with arcpy.da.SearchCursor(fc,['NAME','SHAPE@']) as cursor:
+    for row in cursor:
+        print row[0],row[1].extent.XMin,row[1].extent.XMax,row[1].extent.YMin,row[1].extent.YMax
+
+#remove field
+arcpy.DeleteField_management(fc,'NAME')
+
+#add field
+arcpy.AddField_management(fc,'NAME','Text',None,None,50)
+
+#calculate field
+arcpy.CalculateField_management(fc,'NAME','[_NAME]')
+
+#list fields
+fields = arcpy.ListFields(fc,None,None)
+for fld in fields:
+    print fld.name,fld.type,fld.length
+
+
+
+
+
+
+
 
 
